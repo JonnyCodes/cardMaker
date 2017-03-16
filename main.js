cce9b817c577e438ccdf994ca5541f5137f3abc3
Jonny Blackburn <JBlackbburn89@gmail.com>
2017-03-16 11:55:17 +0000

    First commit. Done most of it, just need to fix some of the styling and a problem with the number of cards not fitting on the second page and after

---
+++ main.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+	$('select').material_select();
+
+	$('.switch').on('click', function() { $('.collapsible').trigger('click'); });
+
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		// Great success! All the File APIs are supported.
+		$("#fileinput").change(handleFilesSelected);
+		$("#applyBtn").click(applyStyles);
+	} else {
+	  alert('The File APIs are not fully supported in this browser.');
+	}
+});
+
+function handleFilesSelected(event) {
+	let files = event.target.files;
+
+	if(files.length > 0) {		
+		if($("#imageArea").children().length > 0) {
+			$("#imageArea").empty();
+		}
+
+		for (var i = files.length - 1; i >= 0; i--) {
+			loadFile(files[i]);
+		}
+	}
+}
+
+function loadFile(file) {
+	let fileReader = new FileReader();
+	fileReader.addEventListener("load", onLoaded);
+	fileReader.readAsDataURL(file);
+}
+
+function onLoaded(event) {
+	$("#imageArea").append($("<img>").attr({src: event.target.result}));
+}
+
+function applyStyles(event) {
+	let resizeCards = $("#shouldResize").is(':checked');
+	let cardSize = $("#cardSizes").val();
+
+	let applyBorder = $("#applyBorder").is(':checked');
+	let borderColor = applyBorder ? $("#borderColor").val() : "#000000";
+	let borderThickness = applyBorder ? $("#borderThickness").val() : 0;
+
+	let rowSpace = $("#rowSpace").val();
+	let columnSpace = $("#columnSpace").val();
+
+	let newWidth;
+	let newHeight;
+
+	if(resizeCards) {
+		switch(cardSize) {
+			case "poker":
+				newWidth = 2.55;
+				newHeight = 3.54;
+			break;
+
+			case "bridge":
+				newWidth = 2.25;
+				newHeight = 3.5;
+			break;
+
+			case "large":
+				newWidth = 3.5;
+				newHeight = 5;
+			break;
+
+			case "tarot":
+				newWidth = 2.75;
+				newHeight = 4.75;
+			break;
+
+			case "mini":
+				newWidth = 1.75;
+				newHeight = 2.5;
+			break;
+		}
+	}
+
+	$("#imageArea").children().css({
+		"border":  `${borderThickness}px solid ${borderColor}`,
+		"margin-right": `${columnSpace}px`,
+		"margin-bottom": `${rowSpace}px`,
+		"width": resizeCards ? newWidth + "in" : "",
+		"height": resizeCards ? newHeight + "in" : ""
+	});
+}
